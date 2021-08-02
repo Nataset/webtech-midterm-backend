32b0c6c41e8cf2e8df8ae547c432cd8fa2ab3f1c
Nataset Tanabodee <nataset.t@ku.th>
2021-08-02 14:42:58 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e261f3d47ba10d205abbce7709a050e4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || "851add46-a42c-4a08-89a6-c4e09957a85e",
+};
